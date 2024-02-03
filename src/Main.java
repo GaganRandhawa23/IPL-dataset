d4193e2b3c89729a01265ed103a14e9190e441b9
Gagan Randhawa <gagan23randhawa@gmail.com>
2024-02-03 12:37:14 +0530

    removed the hard code elements

--- src/Main.java
+++ src/Main.java
@@ -2,59 +2,47 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args)
     {
         String line;
-        Matches[] data =new Matches[700];
+        ArrayList <Matches> data = new ArrayList();
 
         try {
             BufferedReader reader = new BufferedReader(new FileReader("matches.csv"));
-            System.out.println(reader.readLine());
+            System.out.println(reader.readLine()); //to ignore first line
             System.out.println("here it begins");
 
-            int i=1;
-            while((line = reader.readLine()) != null)
-            {
-                //System.out.println(line);
-                data[i]= new Matches(line);
-                i++;
+            while ((line = reader.readLine()) != null) {
+                // System.out.println(line);
+                data.add(new Matches(line));
             }
-            System.out.println( reader.readLine());
+            //System.out.println( reader.readLine());
             reader.close();
         }
         catch(IOException e)
         {
             e.printStackTrace();
         }
-//        for(int i =1; i<data.length; i++)
-//        {
-//            System.out.println(data[i].id);
-//        }
 
-
-        matches_per_year(data,2017);
+        matchesPerYear(data);
     }
-    public static void matches_per_year(Matches[] data,int year)
-    {
-        HashMap <Integer,Integer>map =  new HashMap<>();
-        int i =1;
-        while(i<637)
-        {
-            map.put(data[i].id, data[i].year);
-            i++;
+
+
+    public static void matchesPerYear(ArrayList<Matches> dataList) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (Matches match : dataList) {
+            map.put(match.year, map.getOrDefault(match.year, 0) + 1);
         }
 
-        int count = 0;
+        System.out.println("Q1.) Number of matches played per year in IPL:");
 
-        for (int y : map.values()) {
-            if (y == year) {
-                count++;
-            }
+        for (int year : map.keySet()) {
+            System.out.println("Year " + year + ": " + map.get(year) + " matches");
         }
-
-        System.out.println(count);
     }
 }
