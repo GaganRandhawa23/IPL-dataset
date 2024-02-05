af2a16b115e3f61c3bd106413a4a3fbda878e6d2
Gagan Randhawa <gagan23randhawa@gmail.com>
2024-02-05 22:01:29 +0530

    rewritten acc. to standards

--- src/Main.java
+++ src/Main.java
@@ -1,65 +1,230 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
-    public static void main(String[] args)
-    {
-        //matches
+    public static final int MATCH_ID = 0;
+    public static final int MATCH_YEAR = 1;
+    public static final int MATCH_CITY = 2;
+    public static final int MATCH_DATE = 3;
+    public static final int MATCH_TEAM1 = 4;
+    public static final int MATCH_TEAM2 = 5;
+    public static final int MATCH_TOSS_WINNER = 6;
+    public static final int MATCH_TOSS_DECISION = 7;
+    public static final int MATCH_RESULT = 8;
+    public static final int MATCH_DL = 9;
+    public static final int MATCH_WINNER = 10;
+    public static final int MATCH_WIN_BY_RUNS = 11;
+    public static final int MATCH_WIN_BY_WICKETS = 12;
+    public static final int MATCH_PLAYER = 13;
+    public static final int MATCH_VENUE = 14;
+    public static final int MATCH_UMPIRE1 = 15;
+    public static final int MATCH_UMPIRE2 = 16;
+    public static final int DELIVERY_INNING = 1;
+    public static final int DELIVERY_BATTING_TEAM = 2;
+    public static final int DELIVERY_BOWLING_TEAM = 3;
+    public static final int DELIVERY_OVER = 4;
+    public static final int DELIVERY_BALL = 5;
+    public static final int DELIVERY_BATSMAN = 6;
+    public static final int DELIVERY_NON_STRIKER = 7;
+    public static final int DELIVERY_BOWLER = 8;
+    public static final int DELIVERY_IS_SUPER_OVER = 9;
+    public static final int DELIVERY_RUNS = 10;
+    public static final int DELIVERY_BYE_RUNS = 11;
+    public static final int DELIVERY_LEGBYE_RUNS = 12;
+    public static final int DELIVERY_NOBALL_RUNS = 13;
+    public static final int DELIVERY_PENALTY_RUNS = 14;
+    public static final int DELIVERY_BATSMAN_RUNS = 15;
+    public static final int DELIVERY_EXTRA_RUNS = 16;
+    public static final int DELIVERY_TOTAL_RUNS = 17;
+    public static final int DELIVERY_PLAYER_DISMISSED = 18;
+    public static final int DELIVERY_DISMISSAL_KIND = 19;
+    public static final int DELIVERY_FIELDER = 20;
 
+    public static void main(String[] args) {
+        ArrayList<Match> matches = getMatchesData();
+        ArrayList<Delivery> data2 = getDeliveriesData();
+
+        matchesPerYear(matches);
+        teamWins(matches);
+        extraRuns(matches, data2, 2016);
+        topBowler(matches, data2, 2015);
+
+    }
+
+    private static ArrayList<Match> getMatchesData() {
         String line;
-        ArrayList <Matches> data = new ArrayList<>();
+        ArrayList<Match> matches = new ArrayList<>();
 
         try {
             BufferedReader reader = new BufferedReader(new FileReader("/home/gagan/IdeaProjects/IPL/matches.csv"));
-            System.out.println(reader.readLine()); //to ignore first line
+            String fileHeading = reader.readLine();
 
-            while ((line = reader.readLine()) != null)
-            {
-                // System.out.println(line);
-                data.add(new Matches(line));
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)|(?<=\")(,)(?=\")");
+                Match match = new Match();
+
+                match.setId(Integer.parseInt(parts[MATCH_ID]));
+                match.setYear(Integer.parseInt(parts[MATCH_YEAR]));
+                match.setCity(parts[MATCH_CITY]);
+                match.setDate(parts[MATCH_DATE]);
+                match.setTeam1(parts[MATCH_TEAM1]);
+                match.setTeam2(parts[MATCH_TEAM2]);
+                match.setToss_winner(parts[MATCH_TOSS_WINNER]);
+                match.setToss_decision(parts[MATCH_TOSS_DECISION]);
+                match.setResult(parts[MATCH_RESULT]);
+                match.setDl(parts[MATCH_DL]);
+                match.setWinner(parts[MATCH_WINNER]);
+                match.setWins_by_run(Integer.parseInt(parts[MATCH_WIN_BY_RUNS]));
+                match.setWins_by_wicket(Integer.parseInt(parts[MATCH_WIN_BY_WICKETS]));
+                match.setPlayer(parts[MATCH_PLAYER]);
+                match.setVenue(parts[MATCH_VENUE]);
+                if (parts.length > 15) {
+                    match.setUmpire1(parts[MATCH_UMPIRE1]);
+                    if (parts.length > 16) {
+                        match.setUmpire2(parts[MATCH_UMPIRE2]);
+                    } else {
+                        match.setUmpire2(null);
+                    }
+                } else {
+                    match.setUmpire1(null); // Set umpire1 to null if not present
+                    match.setUmpire2(null); // Set umpire2 to null if not present
+                }
+                matches.add(match);
             }
-            //System.out.println( reader.readLine());
             reader.close();
-        }
-        catch(IOException e)
-        {
+        } catch (IOException e) {
             e.printStackTrace();
         }
+        return matches;
+    }
 
-        // deliveries
-
-        String line2;
-        ArrayList <Deliveries> data2 = new ArrayList();
+    private static ArrayList<Delivery> getDeliveriesData() {
+        String line;
+        ArrayList<Delivery> deliveries = new ArrayList<>();
 
         try {
             BufferedReader reader = new BufferedReader(new FileReader("/home/gagan/IdeaProjects/IPL/deliveries.csv"));
-            System.out.println(reader.readLine()); //to ignore first line
+            String fileHeading = reader.readLine();
 
-            while ((line = reader.readLine()) != null)
-            {
-                // System.out.println(line);
-                data2.add(new Deliveries(line));
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(",");
+                Delivery delivery = new Delivery();
+                delivery.setMatch_id(Integer.parseInt(parts[MATCH_ID]));
+                delivery.setInning(Integer.parseInt(parts[DELIVERY_INNING]));
+                delivery.setBatting_team(parts[DELIVERY_BATTING_TEAM]);
+                delivery.setBowling_team(parts[DELIVERY_BOWLING_TEAM]);
+                delivery.setOver(Integer.parseInt(parts[DELIVERY_OVER]));
+                delivery.setBall(Integer.parseInt(parts[DELIVERY_BALL]));
+                delivery.setBatsman(parts[DELIVERY_BATSMAN]);
+                delivery.setNon_striker(parts[DELIVERY_NON_STRIKER]);
+                delivery.setBowler(parts[DELIVERY_BOWLER]);
+                delivery.setIs_super_over(Integer.parseInt(parts[DELIVERY_IS_SUPER_OVER]));
+                delivery.setWide_runs(Integer.parseInt(parts[DELIVERY_RUNS]));
+                delivery.setBye_runs(Integer.parseInt(parts[DELIVERY_BYE_RUNS]));
+                delivery.setLegbye_runs(Integer.parseInt(parts[DELIVERY_LEGBYE_RUNS]));
+                delivery.setNoball_runs(Integer.parseInt(parts[DELIVERY_NOBALL_RUNS]));
+                delivery.setPenalty_runs(Integer.parseInt(parts[DELIVERY_PENALTY_RUNS]));
+                delivery.setBatsman_runs(Integer.parseInt(parts[DELIVERY_BATSMAN_RUNS]));
+                delivery.setExtra_runs(Integer.parseInt(parts[DELIVERY_EXTRA_RUNS]));
+                delivery.setTotal_runs(Integer.parseInt(parts[DELIVERY_TOTAL_RUNS]));
+                delivery.setPlayer_dismissed((parts.length > 18) ? parts[DELIVERY_PLAYER_DISMISSED] : null);
+                delivery.setDismissal_kind((parts.length > 19) ? parts[DELIVERY_DISMISSAL_KIND] : null);
+                delivery.setFielder((parts.length > 20) ? parts[DELIVERY_FIELDER] : null);
+
+                deliveries.add(delivery);
             }
-            //System.out.println( reader.readLine());
             reader.close();
-        }
-        catch(IOException e)
-        {
+        } catch (IOException e) {
             e.printStackTrace();
         }
+        return deliveries;
 
+    }
 
-        // questions
+    private static void matchesPerYear(ArrayList<Match> matches) {
+        HashMap<Integer, Integer> map = new HashMap<>();
 
-        DataProcessor.matchesPerYear(data);
-            System.out.println("\n");
-        DataProcessor.teamWins(data);
-            System.out.println("\n");
-        DataProcessor.extraRuns(data,data2,2016);
-            System.out.println("\n");
-        DataProcessor.topBowler(data,data2,2015);
+        for (Match match : matches) {
+            map.put(match.getYear(), map.getOrDefault(match.getYear(), 0) + 1);
+        }
+        System.out.println("Q1.) Number of matches played per year in IPL:");
+        System.out.println(map);
+    }
 
+    private static void teamWins(ArrayList<Match> matches) {
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for (Match match : matches) {
+            String winner;
+            if (match.getWinner().isEmpty()) {
+                winner = "No Result";
+            } else {
+                winner = match.getWinner();
+            }
+            map.put(winner, map.getOrDefault(winner, 0) + 1);
+        }
+        System.out.println("Q2.) Number of matches won of all teams over all the years of IPL.");
+        System.out.println(map);
+    }
+
+    public static void extraRuns(ArrayList<Match> matches, ArrayList<Delivery> deliveries, int year) {
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for (Delivery delivery : deliveries) {
+            if (isMatcingYear(matches, year, delivery)) {
+                map.put(delivery.getBowling_team(), map.getOrDefault(delivery.getBowling_team(), 0) + delivery.getExtra_runs());
+            }
+        }
+        System.out.println("Q3.) Extra runs for the year " + year + ":");
+        System.out.println(map);
+    }
+
+    public static void topBowler(ArrayList<Match> matches, ArrayList<Delivery> deliveries, int year) {
+        HashMap<String, Integer> runs = new HashMap<>();
+        HashMap<String, Integer> valid_balls = new HashMap<>();
+
+        for (Delivery delivery : deliveries) {
+            if (isMatcingYear(matches, year, delivery)) {
+                runs.put(delivery.getBowler(), runs.getOrDefault(delivery.getBowler(), 0) + delivery.getTotal_runs());
+            }
+        }
+
+        for (Delivery delivery : deliveries) {
+            if (isMatcingYear(matches, year, delivery) && delivery.getNoball_runs() == 0 && delivery.getWide_runs() == 0) {
+                valid_balls.put(delivery.getBowler(), valid_balls.getOrDefault(delivery.getBowler(), 0) + 1);
+            }
+        }
+
+        HashMap<String, Double> economy = new HashMap<>();
+        Set<String> keys = runs.keySet();
+        for (String key : keys) {
+            int total_runs = runs.get(key);
+            float overs = (float)(valid_balls.get(key)) / 6;
+
+            economy.put(key, (double) (total_runs / overs));
+        }
+
+        List<Map.Entry<String, Double>> sortedEntries = economy.entrySet().stream().sorted(Map.Entry.<String, Double>comparingByValue()).collect(Collectors.toList());
+
+        System.out.println("Q.4) Top 10 Economical Bowlers: ");
+        int count = 0;
+        for (Map.Entry<String, Double> entry : sortedEntries) {
+            if (count < 10) {
+                System.out.print(entry + ", ");
+                count++;
+            } else {
+                break;
+            }
+        }
+    }
+
+    private static boolean isMatcingYear(ArrayList<Match> data, int year, Delivery delivery) {
+        for (Match match : data) {
+            if (match.getYear() == year && (match.getId()) == (delivery.getMatch_id())) {
+                return true;
+            }
+        }
+        return false;
     }
 }
---
+++ src/Match.java
@@ -0,0 +1,138 @@
+public class Match {
+    private int id, year, wins_by_run, wins_by_wicket;
+    private String city, date, team1, team2, toss_winner, toss_decision, result, dl, winner, player, venue, umpire1, umpire2;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getYear() { return year; }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getWins_by_run() {
+        return wins_by_run;
+    }
+
+    public void setWins_by_run(int wins_by_run) {
+        this.wins_by_run = wins_by_run;
+    }
+
+    public int getWins_by_wicket() {
+        return wins_by_wicket;
+    }
+
+    public void setWins_by_wicket(int wins_by_wicket) {
+        this.wins_by_wicket = wins_by_wicket;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTeam1() {
+        return team1;
+    }
+
+    public void setTeam1(String team1) {
+        this.team1 = team1;
+    }
+
+    public String getTeam2() {
+        return team2;
+    }
+
+    public void setTeam2(String team2) {
+        this.team2 = team2;
+    }
+
+    public String getToss_winner() {
+        return toss_winner;
+    }
+
+    public void setToss_winner(String toss_winner) {
+        this.toss_winner = toss_winner;
+    }
+
+    public String getToss_decision() {
+        return toss_decision;
+    }
+
+    public void setToss_decision(String toss_decision) {
+        this.toss_decision = toss_decision;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public String getDl() {
+        return dl;
+    }
+
+    public void setDl(String dl) {
+        this.dl = dl;
+    }
+
+    public String getWinner() {
+        return winner;
+    }
+
+    public void setWinner(String winner) {
+        this.winner = winner;
+    }
+
+    public String getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(String player) {
+        this.player = player;
+    }
+
+    public String getVenue() {
+        return venue;
+    }
+
+    public void setVenue(String venue) {
+        this.venue = venue;
+    }
+
+    public String getUmpire1() {
+        return umpire1;
+    }
+
+    public void setUmpire1(String umpire1) {
+        this.umpire1 = umpire1;
+    }
+
+    public String getUmpire2() {
+        return umpire2;
+    }
+
+    public void setUmpire2(String umpire2) {
+        this.umpire2 = umpire2;
+    }
+}
